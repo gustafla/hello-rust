3f9c5f0627848134600c0bafc9b11247f0195f5f
Lauri Gustafsson <luutifa@gmail.com>
2018-03-22 18:24:40 +0200

    Fix an oversight in VA main comment and use std::f32::consts::PI

--- vector_arithmetic/src/main.rs
+++ vector_arithmetic/src/main.rs
@@ -1,9 +1,11 @@
 mod va; // "Include" src/va.rs
 
-fn main() {
-    let pi = 3.14159265_f32;
+use std::f32;
 
-    // Define two points across an imaginary circle with r=2
+fn main() {
+    let pi = f32::consts::PI;
+
+    // Define two points across an imaginary circle with r=1
     let point_a = vec![(pi/4f32).cos(), (pi/4f32).sin()];
     let point_b = vec![((pi/4f32)*5f32).cos(), ((pi/4f32)*5f32).sin()];
     println!("{:?} and {:?}", point_a, point_b);
