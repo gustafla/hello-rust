e3c4bd82e3fdb3da4bdbe04ef71b304c08a67762
Lauri Gustafsson <luutifa@gmail.com>
2018-03-22 18:18:30 +0200

    project "structs" is now a vector arithmetic test :D

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,21 +1,19 @@
-mod vec; // "Include" src/vec.rs
-
-//#[derive(Debug)]
-//struct Rectangle {
-//    a: Vec2,
-//    b: Vec2,
-//}
-//
-//impl Rectangle {
-//    fn area(self) -> f32 {
-//        // destructuring
-//        let d: Vec2 = self.b - self.a;
-//        d.x * d.y
-//    }
-//}
+mod va; // "Include" src/va.rs
 
 fn main() {
-    let point = vec![0.1_f32, 0.5_f32];
-    println!("{:?}", point);
-    println!("{}", vec::distance(&point, &vec![0.6_f32, 1.3_f32]).unwrap());
+    let pi = 3.14159265_f32;
+
+    // Define two points across an imaginary circle with r=2
+    let point_a = vec![(pi/4f32).cos(), (pi/4f32).sin()];
+    let point_b = vec![((pi/4f32)*5f32).cos(), ((pi/4f32)*5f32).sin()];
+    println!("{:?} and {:?}", point_a, point_b);
+
+    // Should print "distance 2" (r * 2)
+    println!("distance {}", va::distance(&point_a, &point_b));
+
+    // Test arithmetic
+    println!("{:?}", va::add(&vec![0_f32, 0_f32], &vec![1_f32, -1_f32]));
+    println!("{:?}", va::sub(&vec![0_f32, 0_f32], &vec![1_f32, -1_f32]));
+    println!("{:?}", va::mul(&vec![1_f32, 1_f32], &vec![2_f32, -2_f32]));
+    println!("{:?}", va::div(&vec![1_f32, 1_f32], &vec![2_f32, -2_f32]));
 }
---
+++ structs/src/va.rs
@@ -0,0 +1,44 @@
+use std::vec::Vec;
+use std::cmp;
+
+fn operate<F: Fn(&f32, &f32) -> f32>(a: &[f32], b: &[f32], op: F) -> Vec<f32> {
+    // <https://users.rust-lang.org/t/how-to-zip-two-slices-efficiently/2048>
+
+    // Allocate a result
+    let len = cmp::min(a.len(), b.len());
+    let mut result = Vec::with_capacity(len);
+
+    // Limit slices by shadowing their variables for optimizer
+    let a = &a[..len];
+    let b = &b[..len];
+
+    for i in 0..len {
+        result.push(op(&a[i], &b[i]));
+    }
+
+    result
+}
+
+pub fn add(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+    operate(a.as_slice(), b.as_slice(), |a, b| a + b)
+}
+
+pub fn sub(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+    operate(a.as_slice(), b.as_slice(), |a, b| a - b)
+}
+
+pub fn mul(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+    operate(a.as_slice(), b.as_slice(), |a, b| a * b)
+}
+
+pub fn div(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+    operate(a.as_slice(), b.as_slice(), |a, b| a / b)
+}
+
+pub fn length(v: &Vec<f32>) -> f32 {
+    v.iter().fold(0., |sum, &val| sum + val.powi(2)).sqrt()
+}
+
+pub fn distance(a: &Vec<f32>, b: &Vec<f32>) -> f32 {
+    length(&sub(a, b))
+}
